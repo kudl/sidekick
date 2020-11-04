0850262f84c1d320188ff0ad06103565035adc88
kudl <yoocm1229@naver.com>
2020-11-04 16:00:24 +0900

    ADD sample code

---
+++ src/main/java/com/example/sidekick/common/SidekickConstant.java
@@ -0,0 +1,9 @@
+package com.example.sidekick.common;
+
+public class SidekickConstant {
+
+	public static class URL {
+		public static final String BASE = "/";
+		public static final String SIDEKIQ = BASE + "hello";
+	}
+}
--- src/main/java/com/example/sidekick/controller/SidekickController.java
+++ src/main/java/com/example/sidekick/controller/SidekickController.java
@@ -1,12 +1,16 @@
 package com.example.sidekick.controller;
 
+import com.example.sidekick.common.SidekickConstant;
 import com.example.sidekick.config.AppProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.example.sidekick.common.SidekickConstant.URL.SIDEKIQ;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping(SidekickConstant.URL.BASE)
 public class SidekickController {
 
 	private final AppProperties.App appProperties;
@@ -18,5 +22,15 @@ public class SidekickController {
 	@GetMapping
 	public String getProperties() {
 		return String.format("Name : " + appProperties.getName() + " ReadTimeout : " + appProperties.getReadTimeout());
+	}
+
+	@GetMapping(value = SIDEKIQ)
+	public String getSidekiq() {
+		return "Get Sidekiq";
+	}
+
+	@PostMapping(value = SIDEKIQ)
+	public String createSidekiq() {
+		return "Created Sidekiq";
 	}
 }
