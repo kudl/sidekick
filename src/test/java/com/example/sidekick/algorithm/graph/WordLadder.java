2d2e7a0c9ab6b310e93781d39de9342d7582e006
kudl <yoocm1229@naver.com>
2020-11-06 16:50:56 +0900

    ADD algorithm

--- src/test/java/com/example/sidekick/algorithm/graph/WordLadder.java
+++ src/test/java/com/example/sidekick/algorithm/graph/WordLadder.java
@@ -2,29 +2,45 @@ package com.example.sidekick.algorithm.graph;
 
 import java.util.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class WordLadder {
 
 	public static void main(String[] args) {
-		String[] words = {"hot", "dot", "lot", "log", "cog"};
-		List<String> wordList = Arrays.asList(words);
-
 		WordLadder wordLadder = new WordLadder();
-		System.out.println(wordLadder.solution("hit", "cog", wordList));
+
+		String beginV1 = "hit";
+		String targetV1 = "cog";
+		String[] wordsV1 = {"hot", "dot", "lot", "log", "cog"};
+		int answerV1 = 4;
+
+		String beginV2 = "hit";
+		String targetV2 = "cog";
+		String[] wordsV2 = {"hot", "dot", "dog", "lot", "log"};
+		int answerV2 = 0;
+
+		assertAll("WordLadder",
+				() -> assertThat(wordLadder.solution(beginV1, targetV1, wordsV1)).isEqualTo(answerV1),
+				() -> assertThat(wordLadder.solution(beginV2, targetV2, wordsV2)).isEqualTo(answerV2)
+		);
 	}
 
-	private int solution(String beginWord, String endWord, List<String> wordList) {
-		if (wordList == null || !wordList.contains(endWord)) {
+	private int solution(String begin, String target, String[] words) {
+		List<String> wordList = Arrays.asList(words);
+
+		if (wordList == null || !wordList.contains(target)) {
 			return 0;
 		}
 
 		Queue<String> queue = new LinkedList<>();
 		Set<String> dict = new HashSet<>(wordList);
 
-		queue.offer(beginWord);
-		dict.add(endWord);
-		dict.remove(beginWord);
+		queue.offer(begin);
+		dict.add(target);
+		dict.remove(begin);
 
-		int level = 1;
+		int level = 0;
 
 		while (!queue.isEmpty()) {
 			int size = queue.size();
@@ -32,7 +48,7 @@ public class WordLadder {
 			for (int i = 0; i < size; i++) {
 				String str = queue.poll();
 
-				if (str.equals(endWord)) {
+				if (str.equals(target)) {
 					return level;
 				}
 
