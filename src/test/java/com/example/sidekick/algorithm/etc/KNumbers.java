155a1ad2b042c0c53d7865aea7c5504bbf650ed5
kudl <yoocm1229@naver.com>
2020-11-11 16:17:41 +0900

    ADD algorithm

---
+++ src/test/java/com/example/sidekick/algorithm/etc/KNumbers.java
@@ -0,0 +1,30 @@
+package com.example.sidekick.algorithm.etc;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KNumbers {
+
+	public static void main(String[] args) {
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {
+			{2, 5, 3},{4, 4, 1},{1, 7, 3}
+		};
+		int[] answer = {5,6,3};
+
+		assertThat(new KNumbers().solution(array, commands)).isEqualTo(answer);
+	}
+
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+
+		for (int i = 0; i < commands.length; i++) {
+			int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+			Arrays.sort(temp);
+			answer[i] = temp[commands[i][2] - 1];
+		}
+
+		return answer;
+	}
+}
---
+++ src/test/java/com/example/sidekick/algorithm/etc/NoSameNumber.java
@@ -0,0 +1,44 @@
+package com.example.sidekick.algorithm.etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class NoSameNumber {
+
+	public static void main(String[] args) {
+		int[] arr1 = {1, 1, 3, 3, 0, 1, 1};
+		int[] answer1 = {1, 3, 0, 1};
+
+		int[] arr2 = {4, 4, 4, 3, 3};
+		int[] answer2 = {4, 3};
+
+		NoSameNumber noSameNumber = new NoSameNumber();
+		assertAll("NoSameNumber",
+				() -> assertThat(noSameNumber.solution(arr1)).isEqualTo(answer1),
+				() -> assertThat(noSameNumber.solution(arr2)).isEqualTo(answer2)
+		);
+	}
+
+	public int[] solution(int[] arr) {
+		List<Integer> arrayList = new ArrayList<>();
+		int preNum = 10;
+
+		for (int num : arr) {
+			if (preNum != num) {
+				arrayList.add(num);
+			}
+			preNum = num;
+		}
+
+		int[] answer = new int[arrayList.size()];
+
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = arrayList.get(i);
+		}
+
+		return answer;
+	}
+}
